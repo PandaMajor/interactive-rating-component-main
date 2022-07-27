9dc6ca2b97fdbf7a8f18bb9147b3b5e62d029b49
Major Liu <majorliu99@gmail.com>
2022-07-26 19:44:37 -0500

    finished javascript component, added thank you card
    finished the javascript component, added thank you card

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,12 +1,43 @@
-ratings = document.getElementsByClassName("rating");
+// Globals 
+const ratings = document.getElementsByClassName("rating");  // Array of rating buttons
+var selection = 0;                                          // Selected rating
 
-function selectRating(selection){
-    console.log("click detected!");
-    console.log(selection);
+// Initial function called on page load, adds listeners to buttons
+function init(){
+    // Add the callback function submission to the submit button
+    const submitBtn = document.getElementById("submit");
+    submitBtn.addEventListener("click", function() {
+        submission();
+    });
+
+    // Add the callback function selectRating to the individual ratings
+    for(let i = 0; i < ratings.length; i++){
+        ratings[i].addEventListener("click", function() {
+            selectRating(ratings[i]);
+        });
+    }
 }
 
-for (rating in ratings) {
-    console.log(rating);
-    rating.onclick = function(){selectRating(this);};
+/*  Changes the user-select display text based on the selected rating.
+    Hides the prompt card and replaces it with the thank you card. */
+function submission() {
+    document.getElementById("user-select").innerText = 'You selected ' + selection + ' out of 5';
+    document.getElementById("prompt").style.display = "none";
+    document.getElementById("thank-you").style.display = "flex";
 }
-console.log(ratings);
+
+/*  Changes the background color of the selected rating, ensures only one
+    can be orange at a time */
+function selectRating(rating) {
+    for(let i = 0; i < ratings.length; i++){
+        if(ratings[i].innerHTML != rating.innerHTML){
+            ratings[i].style.background = "hsl(210, 19%, 24%)";
+            ratings[i].style.color = "hsl(216, 8%, 52%)";
+        }
+        else{
+            rating.style.background = "hsl(25, 97%, 53%)";
+            rating.style.color = "hsl(0, 0%, 100%)";
+            selection = rating.innerHTML;
+        }
+    }
+}
